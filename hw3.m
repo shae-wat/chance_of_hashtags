f3a818c33cecf4cc0e4572f6bf7ba0aadf30a2fe
Shaelyn Watson <shaelynjoy@gmail.com>
2014-04-04 15:01:49 -0500

    fixed syntax errors

--- hw3.m
+++ hw3.m
@@ -8,7 +8,7 @@ m=4635; %number movies
 k = 10; %number genres
 iterations = 30;
 lambda = 0.5; %****
-I = eye(k,k) %****
+Iu = eye(k,k) %****
 
 %=======Learn M and U given R=======
 
@@ -18,10 +18,10 @@ M = randn(k,m);
 %Alternating minimization
 
 for i=1:iterations
-    if(i%2 == 0) %update M
-        M = inv(U'*U + lambda*I)*U'*Ratings
+    if(mod(i,2) == 0) %update M
+        M = inverse(U'*U + lambda*I)*U'*Ratings
     else %update U
-        U = inv(M'*M + lambda*I)*M'*Ratings
+        U = inverse(M'*M + lambda*I)*M'*Ratings
     end
 end
 
