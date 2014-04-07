4ff4748eb676cfe22cf011dfd47d269fa5e939bd
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-07 15:35:49 -0500

    debugging over ssh

--- hw3.m
+++ hw3.m
@@ -5,30 +5,30 @@ load hw3_netflix.mat
 %alt_min.m contains learning function
 
 %Loop over lambdas
-lambdas = [0: 0.05: 1];
-lambda_av_rmse = randn(length(lambdas),1); %hold results
-for l = 1:length(lambdas)
+lambdas = [0: 0.05: 1]
+lambda_av_rmse = randn(length(lambdas),1) %hold results
+%for l = 1:length(lambdas)
 
 	%=======Crossvalidation=======
 
-	total_RMSE = 0;
-	for crossSet = 1:size(cvSet,1)
+%	total_RMSE = 0;
+%	for crossSet = 1:size(cvSet,1)
 		%select cross validation set
-		trRc=trR;
-		trRc(cvSet(crossSet,:))=0;
-		lambda = lambdas(l)
-		[U1,M1] = alt_min(trRc, lambda); %alternating minimization on this set with this lambda value
-		PredictedRatingc = U1*M1';
-		RMSEc = sqrt(sum(sum((PredictedRatingc(cvSet(crossSet,:))-trR(cvSet(crossSet,:))).^2))/length(cvSet(crossSet,:)))
+%		trRc=trR;
+%		trRc(cvSet(crossSet,:))=0;
+%		lambda = lambdas(l)
+%		[U1,M1] = alt_min(trRc, lambda); %alternating minimization on this set with this lambda value
+%		PredictedRatingc = U1*M1';
+%		RMSEc = sqrt(sum(sum((PredictedRatingc(cvSet(crossSet,:))-trR(cvSet(crossSet,:))).^2))/length(cvSet(crossSet,:)))
 	
 		%total RMSE for this lambda
-		total_RMSE = total_RMSE + RMSEc;
-	end
+%		total_RMSE = total_RMSE + RMSEc;
+%	end
 	%average RMSE for this lambda
-	av_RMSE = total_RMSE/10;
-	lambda_av_rmse(l) = av_RMSE;
+%	av_RMSE = total_RMSE/10;
+%	lambda_av_rmse(l) = av_RMSE;
 
-end
+%end
 
 %=======Plot RMSE=======
 
