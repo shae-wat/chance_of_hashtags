808481046282f0d7e532923a8f6f2a8506f55fdc
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-06 22:01:44 -0500

    debugging over ssh

--- hw3.m
+++ hw3.m
@@ -34,6 +34,9 @@ M = randn(m,k);
         for j=4630:size(Ratings,2)
         
             r = Ratings(:,j)
+            for k = 1:length(r)
+                rkkk = r(k)
+            end
             rkj = nonzeros(Ratings(:,j))
         
         
