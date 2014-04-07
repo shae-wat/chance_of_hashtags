85ca8185fe144e7d3792ad37134aaff210a3df20
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-06 22:19:34 -0500

    alternating minimzation working correctly for users and movies

--- hw3.m
+++ hw3.m
@@ -19,40 +19,41 @@ M = randn(m,k);
 
 %Alternating minimization
 
-%for iteration=1:iterations
-    %if(mod(iteration,2) == 0) %=====update U
+for iteration=1:iterations
+    if(mod(iteration,2) == 0) %=====update U
         %for each user
-        %for i=1:size(Ratings,1)
-            %for movie=1:size(Ratings,2)
-                %if(R(i,movie) ~= 0)
-                   % U(i,:) = inverse(nonzeros(M(i,:))'*nonzeros(M(i,:)) + lambda*I)*nonzeros(M(i,:))'*nonzeros(Ratings(i,:))
-                %end
-           % end
-        %end
-    %else %=====update M
+        for i=1:size(Ratings,1)
+            %r=user who rated movie
+            %v=rating
+            [Rr,Rc,Rv] = find(Ratings(i,:));
+            Mk = M(Rr, :);
+            
+            U(i,:) = inv(Mk'*Mk + lambda*I)*Mk'*Rv;
+        end
+    else %=====update M
         %for each movie
-        for j=4630:size(Ratings,2)
+        for j=1:size(Ratings,2)
             %r=user who rated movie
             %v=rating
             [Rr,Rc,Rv] = find(Ratings(:,j));
             Uk = U(Rr, :);
             
-            M(j,:) = inv(Uk'*Uk + lambda*I)*Uk'*Rv
+            M(j,:) = inv(Uk'*Uk + lambda*I)*Uk'*Rv;
         end
-    %end
-%end
+    end
+end
 
 %Predict
 
 PredictedRatings = U*M';
 
-RMSE = sqrt(sum(sum( (PredictedRatings(testIdx)-Ratings(testIdx).^2) )))/length(testIdx);
+%RMSE = sqrt(sum(sum( (PredictedRatings(testIdx)-Ratings(testIdx).^2) )))/length(testIdx);
 
 %=======Crossvalidation=======
 
 %We provide you with a matrix cvSet which contains indices for 10-fold Cross-validation set. Row cvSet(i, :) contains the indices for first cross-validation set. Thus, if you want to leave first set out and perform training on 2-10 sets, then you will type:
-trR1=trR;
-trR1(cvSet(1,:))=0;
+%trR1=trR;
+%trR1(cvSet(1,:))=0;
 
 %=======Plot RMSE=======
 
