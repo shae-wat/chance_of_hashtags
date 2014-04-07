0e502e7b70d79144bdeeba3b6f643d0ec3023b8b
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-06 23:04:53 -0500

    debugging over ssh

--- hw3.m
+++ hw3.m
@@ -7,15 +7,13 @@ u = 1978; %number users
 m=4635; %number movies
 k = 10; %number genres
 iterations = 30;
-lambda = 0.5; %****
-I = eye(k,k); %****
+I = eye(k,k);
 
 %=======Learn M and U given R=======
-
-%R = nonzeros(Ratings)
 U = randn(u,k);
 M = randn(m,k);
 
+lambda = 0.5;
 
 %Alternating minimization
 
@@ -44,16 +42,16 @@ for iteration=1:iterations
 end
 
 %Predict
-
 PredictedRatings = U*M';
 
-RMSE = sqrt(sum(sum( (PredictedRatings(testIdx)-Ratings(testIdx).^2) )))/length(testIdx);
+RMSE = @(sqrt(sum(sum( (PredictedRatings(testIdx)-Ratings(testIdx).^2) )))/length(testIdx));
 
 %=======Crossvalidation=======
 
 %We provide you with a matrix cvSet which contains indices for 10-fold Cross-validation set. Row cvSet(i, :) contains the indices for first cross-validation set. Thus, if you want to leave first set out and perform training on 2-10 sets, then you will type:
-%trR1=trR;
-%trR1(cvSet(1,:))=0;
+trR1=trR;
+trR1(cvSet(1,:))=0;
+vals = crossval(RMSE(trR1))
 
 %=======Plot RMSE=======
 
