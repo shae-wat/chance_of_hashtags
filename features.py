9dc5e4e6fb65e4bc49104bac93331fbc5254dc5e
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-04-26 21:57:36 -0500

    cleaning up tweets

--- features.py
+++ features.py
@@ -1,11 +1,24 @@
 
+#return a dictionary whose keys are features
+#an whose values are true/false
 def tweet_sentiment_features(tweet):
     words = [line.strip().lower() for line in open('sentiment_words.txt')]
  
     features = {}
-    #for each sentiment word
+    #for each sentiment word defined in sentiment_words.txt
     for w in words:
         features['contains(%s)' % w] = (w in tweet)
+        
+        #TODO
+        
+        #remove hashtags
+        #correct mispellings, missing spaces
+        #check context of found words
+        #interpret punctuation
+        
+        
+        
+        
     return features
 
 
--- hashtag_weather.py
+++ hashtag_weather.py
@@ -16,15 +16,44 @@ test = pds.read_csv(paths[1])
 sentiment_featureset = []
 #for each tweet and associated info
 for t in train.iterrows():
+    print "******\n TWEET \n ******\n" + t[1][1] + "\n"    #print tweets
     tweet_words = t[1][1].split()
+
+    print "tweet words before: " + str(tweet_words)
+    for i in range(len(tweet_words)):
+        #normalize words to same case
+        tweet_words[i] = tweet_words[i].lower()
+        #remove hashtag characters from beginning of words
+        if tweet_words[i].startswith("#") or tweet_words[i].startswith("@"): 
+            tweet_words[i] = tweet_words[i][1:]
+        #separate punctuation
+        if "!" in tweet_words[i]:
+            punct_index = tweet_words[i].index("!")
+            tweet_words.append(tweet_words[i][punct_index:])
+            tweet_words[i] = tweet_words[i][:punct_index]
+        if "?" in tweet_words[i]:
+            punct_index = tweet_words[i].index("?")
+            tweet_words.append(tweet_words[i][punct_index:])
+            tweet_words[i] = tweet_words[i][:punct_index]
+        if "." in tweet_words[i]:
+            punct_index = tweet_words[i].index(".")
+            tweet_words.append(tweet_words[i][punct_index:])
+            tweet_words[i] = tweet_words[i][:punct_index]
+        #remove common noise
+        #if "{link}" in tweet_words:
+         #   tweet_words.remove("{link}")
+            
+            
+    #get training classification for this tweet
     sentiment = (t[1][4], t[1][5], t[1][6])
+    print "tweet words after: " + str(tweet_words) + "\n\n"
     #construct sentiment feature set
     sentiment_featureset.append((tweet_sentiment_features(tweet_words), sentiment))
  
     
 #============Bayesian classification============
 
-train_set, test_set = sentiment_featureset[:300], sentiment_featureset[300:600]   
+train_set, test_set = sentiment_featureset[:200], sentiment_featureset[200:400]   
 bayesian_classifier = nl.NaiveBayesClassifier.train(train_set)
 print "bayesian classifier efficiency = " + str(nl.classify.accuracy(bayesian_classifier, test_set)) + "\n"
 bayesian_classifier.show_most_informative_features(5)
