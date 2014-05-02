893a846334f02300a24170cbc17026454f1d217c
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-05-02 12:22:18 -0500

    total and feature counting

--- classifiers.py
+++ classifiers.py
@@ -2,36 +2,14 @@
 
 def bayes_classifier_sentiment(featuresets):
 
-	feature_probabilities = []    #list of tuples [(feature, probability)]
-	s_range_counts = [["s1", [0,0,0,0,0]], ["s2", [0,0,0,0,0]], ["s3", [0,0,0,0,0]], ["s4", [0,0,0,0,0]], ["s5", [0,0,0,0,0]]]
+	s1_range_count = ["s1", [0,0,0,0,0]];
+	s2_range_count = ["s2", [0,0,0,0,0]];
+	s3_range_count = ["s3", [0,0,0,0,0]];
+	s4_range_count = ["s4", [0,0,0,0,0]];
+	s5_range_count = ["s5", [0,0,0,0,0]];
+	s_range_counts = [s1_range_count, s2_range_count, s3_range_count, s4_range_count, s5_range_count]
 
-
-	# n = num instances yj
-	s1_r0 = 0
-	s1_r1 = 0
-	s1_r2 = 0
-	s1_r3 = 0
-	s1_r4 = 0
-	s2_r0 = 0
-	s2_r1 = 0
-	s2_r2 = 0
-	s2_r3 = 0
-	s2_r4 = 0
-	s3_r0 = 0
-	s3_r1 = 0
-	s3_r2 = 0
-	s3_r3 = 0
-	s3_r4 = 0
-	s4_r0 = 0
-	s4_r1 = 0
-	s4_r2 = 0
-	s4_r3 = 0
-	s4_r4 = 0
-	s5_r0 = 0
-	s5_r1 = 0
-	s5_r2 = 0
-	s5_r3 = 0
-	s5_r4 = 0
+	feature_rating_counts = []
 
 	for f in featuresets:
 		print f
@@ -50,7 +28,9 @@ def bayes_classifier_sentiment(featuresets):
 
 		si = 0
 		for s in f[1]: 
-			print "\n" + str(s)
+			#print "\n" + str(s)
+
+			#count total number of occurances
 			if (s_range0[0] <= s < s_range0[1]):
 				s_range_counts[si][1][0] += 1
 			if (s_range1[0] <= s < s_range1[1]):
@@ -62,15 +42,40 @@ def bayes_classifier_sentiment(featuresets):
 			if (s_range4[0] <= s < s_range4[1]):
 				s_range_counts[si][1][4] += 1
 			si += 1
-			print s_range_counts
+			
+
+		#counts occurrances that appear per feature
+		for feature in f[0]:
+			f_s1_range_count = ["s1", [0,0,0,0,0]];
+			f_s2_range_count = ["s2", [0,0,0,0,0]];
+			f_s3_range_count = ["s3", [0,0,0,0,0]];
+			f_s4_range_count = ["s4", [0,0,0,0,0]];
+			f_s5_range_count = ["s5", [0,0,0,0,0]];
+			f_range_count = [f_s1_range_count, f_s2_range_count, f_s3_range_count, f_s4_range_count, f_s5_range_count]
+			
+			si = 0
+			for s in f[1]: 
+				#print "\n" + str(s)
+
+				#count total number of occurances
+				if (s_range0[0] <= s < s_range0[1]):
+					f_range_count[si][1][0] += 1
+				if (s_range1[0] <= s < s_range1[1]):
+					f_range_count[si][1][1] += 1
+				if (s_range2[0] <= s < s_range2[1]):
+					f_range_count[si][1][2] += 1
+				if (s_range3[0] <= s < s_range3[1]):
+					f_range_count[si][1][3] += 1
+				if (s_range4[0] <= s <= s_range4[1]):
+					f_range_count[si][1][4] += 1
+				si += 1
+
+			print f_range_count
+			print feature + "\n"
+			feature_rating_counts.append([feature, f_range_count])
+			print feature_rating_counts
 
 
 
-
-
-	# for f in featuresets:
-	# 	#print f
-	# 	for feature in f[0]:
-	# 		print feature + "\n"
 
 
