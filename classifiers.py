1a7a107ddc12456fcd2744276b7316267295ff3a
Shaelyn-Watson <shaelynjoy@gmail.com>
2014-05-02 12:33:14 -0500

    makes sense

--- classifiers.py
+++ classifiers.py
@@ -2,6 +2,18 @@
 
 def bayes_classifier_sentiment(featuresets):
 
+	# s1 = f[1][0]  #i cant tell
+	# s2 = f[1][1]  #negative
+	# s3 = f[1][2]  #neutral 
+	# s4 = f[1][3]  #positive
+	# s5 = f[1][4]  #not related to weather
+
+	s_range0 = (0,0.2)
+	s_range1 = (0.2,0.4)
+	s_range2 = (0.4,0.6)
+	s_range3 = (0.6,0.8)
+	s_range4 = (0.8,1.0)
+
 	s1_range_count = ["s1", [0,0,0,0,0]];
 	s2_range_count = ["s2", [0,0,0,0,0]];
 	s3_range_count = ["s3", [0,0,0,0,0]];
@@ -14,23 +26,9 @@ def bayes_classifier_sentiment(featuresets):
 	for f in featuresets:
 		print f
 
-		# s1 = f[1][0]  #i cant tell
-		# s2 = f[1][1]  #negative
-		# s3 = f[1][2]  #neutral 
-		# s4 = f[1][3]  #positive
-		# s5 = f[1][4]  #not related to weather
-
-		s_range0 = (0,0.2)
-		s_range1 = (0.2,0.4)
-		s_range2 = (0.4,0.6)
-		s_range3 = (0.6,0.8)
-		s_range4 = (0.8,1.0)
-
-		si = 0
+		#count total number of occurances of s1-s5 ratings per range
+		si=0
 		for s in f[1]: 
-			#print "\n" + str(s)
-
-			#count total number of occurances
 			if (s_range0[0] <= s < s_range0[1]):
 				s_range_counts[si][1][0] += 1
 			if (s_range1[0] <= s < s_range1[1]):
@@ -41,10 +39,10 @@ def bayes_classifier_sentiment(featuresets):
 				s_range_counts[si][1][3] += 1
 			if (s_range4[0] <= s < s_range4[1]):
 				s_range_counts[si][1][4] += 1
-			si += 1
+			si+=1
 			
 
-		#counts occurrances that appear per feature
+		#counts occurrances that appear per feature per range
 		for feature in f[0]:
 			f_s1_range_count = ["s1", [0,0,0,0,0]];
 			f_s2_range_count = ["s2", [0,0,0,0,0]];
@@ -53,6 +51,10 @@ def bayes_classifier_sentiment(featuresets):
 			f_s5_range_count = ["s5", [0,0,0,0,0]];
 			f_range_count = [f_s1_range_count, f_s2_range_count, f_s3_range_count, f_s4_range_count, f_s5_range_count]
 			
+			print feature
+			print f_range_count
+			print "\n"
+
 			si = 0
 			for s in f[1]: 
 				#print "\n" + str(s)
@@ -70,10 +72,11 @@ def bayes_classifier_sentiment(featuresets):
 					f_range_count[si][1][4] += 1
 				si += 1
 
+			print feature
 			print f_range_count
-			print feature + "\n"
+			print "\n"
 			feature_rating_counts.append([feature, f_range_count])
-			print feature_rating_counts
+			#print feature_rating_counts
 
 
 
